08d1e2b524c8f90c86e817dff398f5932e4db894
triadi <triadilaksamana@gmail.com>
2023-05-12 18:56:00 +0700

    refactor: rewrite the eslint and tsconfig files

---
+++ src/main/server.ts
@@ -0,0 +1,2 @@
+require("module-alias/register")
+console.log(`hello world ${1 + 1}`)
--- test/example.test.ts
+++ test/example.test.ts
@@ -1,6 +1,6 @@
-describe('test', () => {
-  it('it should works', () => {
-    expect(true).toBe(true)
-    expect(1).toEqual(1)
-  })
+describe("test", () => {
+    it("it should works", () => {
+        expect(true).toBe(true)
+        expect(1).toEqual(1)
+    })
 })
