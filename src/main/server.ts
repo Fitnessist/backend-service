8ade0588ca0ded48db3aaf480134878839851ef1
triadi <triadilaksamana@gmail.com>
2023-05-21 00:25:01 +0700

    build: add migrate cmd + install deps + set migrat

--- src/main/server.ts
+++
@@ -1,2 +0,0 @@
-require("module-alias/register")
-console.log(`hello world ${1 + 1}`)
--- test/example.test.ts
+++
@@ -1,6 +0,0 @@
-describe("test", () => {
-    it("it should works", () => {
-        expect(true).toBe(true)
-        expect(1).toEqual(1)
-    })
-})
